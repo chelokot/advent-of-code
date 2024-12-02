2e4c0c59ddfc2ab24b3f61e1ac9b470ba951a3b1
chelokot <andrey.vlasenko.work@gmail.com>
2024-12-02 09:19:39 +0200

    more slight cleaning of 2 2

--- 2024/day2/second.py
+++ 2024/day2/second.py
@@ -10,7 +10,7 @@ def is_safe(l, problem_dampening=True):
         if not direction:
             direction = current_direction
         if abs(difference) not in [1, 2, 3] or direction != current_direction:
-            return problem_dampening and (is_safe(l[:i-1] + l[i:], False) or is_safe(l[:i] + l[i+1:], False) or is_safe(l[:i+1] + l[i+2:], False))
+            return problem_dampening and (is_safe(l[:i-1] + l[i:]) or is_safe(l[:i] + l[i+1:]) or is_safe(l[:i+1] + l[i+2:]))
     return True
 
 with open("input.txt") as f:
@@ -19,6 +19,6 @@ with open("input.txt") as f:
 safe_amount = 0
 for line in data:
     report = list(map(int, line.split()))
-    if is_safe(report):
+    if is_safe(report, problem_dampening = True):
         safe_amount += 1
 print(safe_amount)
