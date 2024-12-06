1aac9c492c9d1a78177359ec168f586879e3710a
chelokot <andrey.vlasenko.work@gmail.com>
2024-12-06 07:17:57 +0200

    initial version 6 1

--- 2024/day3/cool_second.py
+++ 2024/day3/cool_second.py
@@ -2,6 +2,6 @@ import re
 with open("input.txt") as f:
     data = f.read()
 
-pattern = r"(?:^|do\(\)).*?(?:mul\((\d{1,3}),(\d{1,3})\).*?)*?(?:$|don't\(\))"
+pattern = r"(?:^|do\(\))(?:.*?)(?:mul\((\d{1,3}),(\d{1,3})\).*?)*?(?:$|don\'t\(\))"
 print(re.findall(pattern, data, re.DOTALL))
 print(sum(map(lambda I: int(I[0]) * int(I[1]),re.findall(pattern, data, re.DOTALL))))
---
+++ 2024/day6/first.py
@@ -0,0 +1,82 @@
+import numpy as np
+
+with open("input.txt") as f:
+    data = f.read().splitlines()
+
+class Lab:
+    def __init__(self, map):
+        self.possible_driections = ['>', 'v', '<', '^']
+        self.map = map
+        for i in range(len(self.map)):
+            for j in range(len(self.map[i])):
+                if self.map[i][j] in self.possible_driections:
+                    self.position = (i, j)
+                    self.direction = self.map[i][j]
+                    break
+        self.visited_positions = set()
+        self.finished = False
+
+    def is_leaving_area(self):
+        i, j = self.position
+        if self.direction == 'v' and i == len(self.map) - 1:
+            return True
+        elif self.direction == '^' and i == 0:
+            return True
+        elif self.direction == '>' and j == len(self.map[i]) - 1:
+           return True
+        elif self.direction == '<' and j == 0:
+            return True
+        return False
+
+    def next_position(self):
+        i, j = self.position
+        if self.direction == 'v':
+            return (i+1, j)
+        elif self.direction == '^':
+            return (i-1, j)
+        elif self.direction == '>':
+            return (i, j+1)
+        elif self.direction == '<':
+            return (i, j-1)
+        else:
+            raise ValueError("Invalid direction")
+
+    def is_path_free(self):
+        i, j = self.position
+        next_i, next_j = self.next_position()
+        if self.map[next_i][next_j] in ['.', 'X']:
+            return True
+        return False
+
+    def step(self):
+        if self.is_leaving_area():
+            self.finished = True
+            return
+        if self.is_path_free():
+            i, j = self.position
+            next_i, next_j = self.next_position()
+            current_state = (self.position, self.direction)
+            if current_state in self.visited_positions:
+                self.finished = True
+                return
+            self.visited_positions.add((self.position, self.direction))
+            self.map[next_i][next_j] = self.direction
+            self.map[i][j] = 'X'
+        else:
+            self.direction = self.possible_driections[(self.possible_driections.index(self.direction) + 1) % 4]
+
+    def count_visited(self):
+        count = 0
+        for i in range(len(self.map)):
+            for j in range(len(self.map[i])):
+                if self.map[i][j] in self.possible_driections + ['X']:
+                    count += 1
+        return count
+
+    def process(self):
+        while not self.finished:
+            self.step()
+        return self.count_visited()
+
+lab = Lab([list(line) for line in data])
+print(lab.process())
