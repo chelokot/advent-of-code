0bdf3de8d118687bf7a95ef0242c3db1c2aa6439
chelokot <andrey.vlasenko.work@gmail.com>
2024-12-12 20:44:41 +0200

    12 2

--- 2024/day12/second.py
+++ 2024/day12/second.py
@@ -5,7 +5,7 @@ class Region:
     def __init__(self, letter):
         self.letter = letter
         self.area = 0
-        self.perimeter = 0
+        self.corners = 0
         self.processed = False
         self.merged_into = None
 
@@ -19,7 +19,7 @@ class Region:
                 return
         print(f'Merging {self} into {other}. Area: {self.area} -> {other.area + self.area}')
         other.add_tile(self.get_area())
-        other.extend_perimeter(self.get_perimeter())
+        other.extend_corners(self.get_corners())
         self.merged_into = other
 
     def get_area(self):
@@ -27,10 +27,10 @@ class Region:
             return self.merged_into.get_area()
         return self.area
 
-    def get_perimeter(self):
+    def get_corners(self):
         if self.merged_into is not None:
-            return self.merged_into.get_perimeter()
-        return self.perimeter
+            return self.merged_into.get_corners()
+        return self.corners
 
     def get_processed(self):
         if self.merged_into is not None:
@@ -45,11 +45,11 @@ class Region:
             print(f'{self} {self.area} -> {self.area + amount}')
         self.area += amount
 
-    def extend_perimeter(self, length):
+    def extend_corners(self, length):
         if self.merged_into is not None:
-            self.merged_into.extend_perimeter(length)
+            self.merged_into.extend_corners(length)
             return
-        self.perimeter += length
+        self.corners += length
 
     def set_processed(self):
         if self.merged_into is not None:
@@ -61,8 +61,10 @@ class Region:
         if self.get_processed():
             return 0
         self.set_processed()
-        print(f"Region {self.letter}: area = {self.get_area()}, perimeter = {self.get_perimeter()}, price = {self.get_area() * self.get_perimeter()}")
-        return self.get_area() * self.get_perimeter()
+        # amount of sides is actually equal to amount of corners!
+        # each corner gives us 2 sides, but each side is shared between 2 corners. So, corners = sides
+        print(f"Region {self.letter}: area = {self.get_area()}, corners = {self.get_corners()}, price = {self.get_area() * self.get_corners()}")
+        return self.get_area() * self.get_corners()
 
 regions = [[None for j in range(len(garden[i]))] for i in range(len(garden))]
 for i in range(len(garden)):
@@ -72,21 +74,30 @@ for i in range(len(garden)):
             neighbour_regions.append(regions[i-1][j])
         if j != 0 and garden[i][j-1] == garden[i][j]:
             neighbour_regions.append(regions[i][j-1])
-        next_neighbours = (i!=len(garden)-1 and garden[i+1][j] == garden[i][j]) + (j!=len(garden[i])-1 and garden[i][j+1] == garden[i][j])
+
+        is_top_left_corner = (i == 0 or garden[i-1][j] != garden[i][j]) and (j == 0 or garden[i][j-1] != garden[i][j])
+        is_top_right_corner = (i == 0 or garden[i-1][j] != garden[i][j]) and (j == len(garden[i]) - 1 or garden[i][j+1] != garden[i][j])
+        is_bottom_left_corner = (i == len(garden) - 1 or garden[i+1][j] != garden[i][j]) and (j == 0 or garden[i][j-1] != garden[i][j])
+        is_bottom_right_corner = (i == len(garden) - 1 or garden[i+1][j] != garden[i][j]) and (j == len(garden[i]) - 1 or garden[i][j+1] != garden[i][j])
+
+        is_internal_top_left_corner = (i != len(garden) - 1 and garden[i+1][j] == garden[i][j]) and (j != len(garden[i]) - 1 and garden[i][j+1] == garden[i][j]) and garden[i+1][j+1] != garden[i][j]
+        is_internal_top_right_corner = (i != len(garden) - 1 and garden[i+1][j] == garden[i][j]) and (j != 0 and garden[i][j-1] == garden[i][j]) and garden[i+1][j-1] != garden[i][j]
+        is_internal_bottom_left_corner = (i != 0 and garden[i-1][j] == garden[i][j]) and (j != len(garden[i]) - 1 and garden[i][j+1] == garden[i][j]) and garden[i-1][j+1] != garden[i][j]
+        is_internal_bottom_right_corner = (i != 0 and garden[i-1][j] == garden[i][j]) and (j != 0 and garden[i][j-1] == garden[i][j]) and garden[i-1][j-1] != garden[i][j]
+
+        new_corners = is_top_left_corner + is_top_right_corner + is_bottom_left_corner + is_bottom_right_corner + is_internal_top_left_corner + is_internal_top_right_corner + is_internal_bottom_left_corner + is_internal_bottom_right_corner
 
         if len(neighbour_regions) == 0:
             regions[i][j] = Region(garden[i][j])
             regions[i][j].add_tile()
-            regions[i][j].extend_perimeter(4 - next_neighbours)
         if len(neighbour_regions) == 1:
             regions[i][j] = neighbour_regions[0]
             regions[i][j].add_tile()
-            regions[i][j].extend_perimeter(4 - next_neighbours - 1)
         if len(neighbour_regions) == 2:
             regions[i][j] = neighbour_regions[0]
             neighbour_regions[1].merge_into(regions[i][j])
             regions[i][j].add_tile()
-            regions[i][j].extend_perimeter(4 - next_neighbours - 2)
+        regions[i][j].extend_corners(new_corners)
 
 price = 0
 for i in range(len(garden)):
