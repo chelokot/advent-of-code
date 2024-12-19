08a4c29d467e70a7b619f126a14d94b64efa16b2
chelokot <andrey.vlasenko.work@gmail.com>
2024-12-19 23:08:12 +0200

    19 1 shortify slightly

--- 2024/day19/first.py
+++ 2024/day19/first.py
@@ -10,11 +10,9 @@ designs = designs.splitlines()
 
 @functools.lru_cache(maxsize=None)
 def check_if_possible(design):
-    print(design)
     for towel in towels:
-        if design.startswith(towel):
-            if check_if_possible(design[len(towel):]):
-                return True
+        if design.startswith(towel) and check_if_possible(design[len(towel):]):
+            return True
     return design == ""
 
 print(sum(tqdm(check_if_possible(design) for design in designs)))
