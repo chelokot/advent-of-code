fc4903845f0f5c73ceccc50d058bb4d4faf3fa42
chelokot <andrii@oriane.xyz>
2025-12-03 12:16:15 +0200

    add 2025 day 3 task 1

---
+++ 2025/day3/first.py
@@ -0,0 +1,14 @@
+with open("input.txt") as f:
+    banks = f.read().splitlines()
+
+def findBestNDigitSubsuqeunce(sequence: str, n: int) -> list[str]:
+    best = max(sequence[: len(sequence) - (n-1)])
+    return [best] + (
+        findBestNDigitSubsuqeunce(sequence[sequence.find(best) + 1 :], n - 1)
+        if n > 1 else []
+    )
+
+def findBiggestNNumber(bank: str, n: int) -> int:
+    return int("".join(findBestNDigitSubsuqeunce(bank, 2)))
+
+print(sum(findBiggestNNumber(bank, 2) for bank in banks))
